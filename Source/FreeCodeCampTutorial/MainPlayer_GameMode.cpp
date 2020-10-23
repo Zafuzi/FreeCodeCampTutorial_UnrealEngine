20cdd709c93f946ae66d89fbc1af78f186433786
Zachary <zacharyfoutz@gmail.com>
2020-10-22 20:16:54 -0700

    Final Commit

--- Source/FreeCodeCampTutorial/MainPlayer_GameMode.cpp
+++ Source/FreeCodeCampTutorial/MainPlayer_GameMode.cpp
@@ -2,4 +2,31 @@
 
 
 #include "MainPlayer_GameMode.h"
+#include "GameFramework/Actor.h"
 
+AMainPlayer_GameMode::AMainPlayer_GameMode() {
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMainPlayer_GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	FTimerHandle UnusedHandle;
+	GetWorldTimerManager().SetTimer(UnusedHandle, this, &AMainPlayer_GameMode::SpawnPlayerRecharge, FMath::RandRange(2.0f, 5.0f), true);
+}
+
+void AMainPlayer_GameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AMainPlayer_GameMode::SpawnPlayerRecharge()
+{
+	float RandX = FMath::RandRange(SpawnX_Min, SpawnX_Max);
+	float RandY = FMath::RandRange(SpawnY_Min, SpawnY_Max);
+
+	FVector SpawnPosition = FVector(RandX, RandY, SpawnZ);
+	FRotator SpawnRotation = FRotator(0.0f, 0.0f, 0.0f);
+
+	GetWorld()->SpawnActor(PlayerRecharge, &SpawnPosition, &SpawnRotation);
+}
--- Source/FreeCodeCampTutorial/MainPlayer_GameMode.h
+++ Source/FreeCodeCampTutorial/MainPlayer_GameMode.h
@@ -6,12 +6,34 @@
 #include "GameFramework/GameMode.h"
 #include "MainPlayer_GameMode.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class FREECODECAMPTUTORIAL_API AMainPlayer_GameMode : public AGameMode
 {
 	GENERATED_BODY()
+
+	AMainPlayer_GameMode();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<APawn> PlayerRecharge;
+
+	float SpawnZ = 500.0f;
+
+	UPROPERTY(EditAnywhere)
+		float SpawnX_Min;
+
+	UPROPERTY(EditAnywhere)
+		float SpawnX_Max;
+
+	UPROPERTY(EditAnywhere)
+		float SpawnY_Min;
+
+	UPROPERTY(EditAnywhere)
+		float SpawnY_Max;
+
+
+	void SpawnPlayerRecharge();
 	
 };
